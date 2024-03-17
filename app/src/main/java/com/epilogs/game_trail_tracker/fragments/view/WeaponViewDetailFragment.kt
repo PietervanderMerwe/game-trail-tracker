604960ddde6ecbe0122f31efb6113af69e38f984
EPi <pietervdm64@gmail.com>
2024-03-17 19:21:22 +0100

    Update view weapon detail

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/view/WeaponViewDetailFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/view/WeaponViewDetailFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.epilogs.game_trail_tracker.R
+import com.epilogs.game_trail_tracker.adapters.ImagesAdapter
+import com.epilogs.game_trail_tracker.viewmodels.WeaponViewModel
 
 class WeaponViewDetailFragment : Fragment() {
     private var weaponId: Int? = null
+    private val viewModel: WeaponViewModel by viewModels()
+    private lateinit var imageAdapter: ImagesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +34,26 @@ class WeaponViewDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_weapon_view_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val name: TextView = view.findViewById(R.id.editTextWeaponNameViewDetail)
+        val notes : TextView = view.findViewById(R.id.editTextWeaponNotesViewDetail)
+        val imagesRecyclerView = view.findViewById<RecyclerView>(R.id.imagesWeaponRecyclerViewViewDetail)
+
+        viewModel.getWeaponById(weaponId!!).observe(viewLifecycleOwner, Observer { weapon ->
+            name.text = weapon?.name
+            notes.text = weapon?.notes
+
+            imageAdapter = ImagesAdapter(mutableListOf())
+            imagesRecyclerView.adapter = imageAdapter
+            imagesRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+            weapon?.imagePaths?.let { imageUrls ->
+                imageAdapter.updateImages(imageUrls)
+            }
+        })
+    }
     companion object {
         @JvmStatic
         fun newInstance(weaponId: Int) =
