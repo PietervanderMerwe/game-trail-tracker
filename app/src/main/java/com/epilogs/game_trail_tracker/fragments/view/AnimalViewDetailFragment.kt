bb006338f95394d03da232ac6c88e5e97e6b2434
EPi <pietervdm64@gmail.com>
2024-03-18 10:45:42 +0100

    Fix

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/view/AnimalViewDetailFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/view/AnimalViewDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.epilogs.game_trail_tracker.fragments.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -52,7 +53,7 @@ class AnimalViewDetailFragment : Fragment() {
         val imagesRecyclerView =
             view.findViewById<RecyclerView>(R.id.imagesAnimalRecyclerViewViewDetail)
 
-        animalViewModel.getAnimalById(id).observe(viewLifecycleOwner, Observer { animal ->
+        animalViewModel.getAnimalById(animalId!!).observe(viewLifecycleOwner, Observer { animal ->
             editTextSpecieName.text = animal?.name
             editTextDate.text = animal?.harvestDate?.let { dateFormat.format(it) } ?: "N/A"
             editTextWeight.text = animal?.weight?.toString()
