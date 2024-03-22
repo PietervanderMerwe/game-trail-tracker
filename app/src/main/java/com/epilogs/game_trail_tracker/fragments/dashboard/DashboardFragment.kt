bd74b2220630094ea8769c067fd7fb2399fe3c0c
Pieter van der Merwe <P.van.der.Merwe@cimsolutions.nl>
2024-03-22 12:33:10 +0100

    Added background image

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/dashboard/DashboardFragment.kt
@@ -11,11 +11,6 @@ import com.epilogs.game_trail_tracker.R
 import com.epilogs.game_trail_tracker.viewmodels.AnimalViewModel
 import com.epilogs.game_trail_tracker.views.TrailView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DashboardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DashboardFragment : Fragment() {
     private lateinit var animalViewModel: AnimalViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +21,6 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
@@ -37,7 +31,7 @@ class DashboardFragment : Fragment() {
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             DashboardFragment().apply {
             }
     }
