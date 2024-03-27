4a0d6cd767a50bff4ed928a0efb0026221e7cef4
Pieter van der Merwe <P.van.der.Merwe@cimsolutions.nl>
2024-03-27 14:53:15 +0100

    Refactor

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/add/AnimalAddFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/add/AnimalAddFragment.kt
@@ -1,11 +1,8 @@
 package com.epilogs.game_trail_tracker.fragments.add
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +11,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Spinner
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -29,35 +28,38 @@ import com.epilogs.game_trail_tracker.database.entities.Location
 import com.epilogs.game_trail_tracker.database.entities.Weapon
 import com.epilogs.game_trail_tracker.fragments.extension.ImageDialogFragment
 import com.epilogs.game_trail_tracker.utils.DateConverter
-import com.epilogs.game_trail_tracker.utils.ImagePickerUtil
 import com.epilogs.game_trail_tracker.utils.showDatePickerDialog
 import com.epilogs.game_trail_tracker.viewmodels.AnimalViewModel
 import com.epilogs.game_trail_tracker.viewmodels.SharedViewModel
-import com.epilogs.game_trail_tracker.viewmodels.WeaponViewModel
 
 class AnimalAddFragment : Fragment() {
     private val viewModel: AnimalViewModel by viewModels()
     private val sharedViewModel: SharedViewModel by activityViewModels()
-    private val pickImagesRequestCode = 100
     private val selectedImageUris = mutableListOf<String>()
     private lateinit var imageAdapter: ImagesAdapter
-    private var locationId: Int? = 0;
-    private var weaponId: Int? = 0;
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<String>
+    private var locationId: Int? = null
+    private var weaponId: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uris ->
+            val imagesUris = uris.map { it.toString() }
+            selectedImageUris.clear()
+            selectedImageUris.addAll(imagesUris)
+            imageAdapter.updateImages(selectedImageUris)
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_animal_add, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUI(view)
+    }
 
+    private fun setupUI(view: View) {
         val editTextSpecieName: EditText = view.findViewById(R.id.editTextSpecieName)
         val editTextDate: EditText = view.findViewById(R.id.editTextDate)
         val editTextWeight: EditText = view.findViewById(R.id.editTextWeight)
@@ -66,6 +68,7 @@ class AnimalAddFragment : Fragment() {
         val spinnerWeapon: Spinner = view.findViewById(R.id.spinnerWeapon)
         val buttonSelectAnimalImages: Button = view.findViewById(R.id.buttonSelectAnimalImages)
         val buttonSaveAnimal: Button = view.findViewById(R.id.buttonSaveAnimal)
+        val imagesRecyclerView: RecyclerView = view.findViewById(R.id.imagesAnimalRecyclerView)
         val dateConverter = DateConverter()
 
         editTextDate.setOnClickListener {
@@ -75,130 +78,12 @@ class AnimalAddFragment : Fragment() {
         }
 
         buttonSelectAnimalImages.setOnClickListener {
-            showImagePicker()
+            imagePickerLauncher.launch("image/*")
         }
 
-        val locations = mutableListOf<Location>()
-        val locationAdapter = LocationAdapter(requireContext(), locations)
-        spinnerLocation.adapter = locationAdapter
-
-        viewModel.getAllLocations().observe(viewLifecycleOwner) { newLocations ->
-            val modifiedLocations = mutableListOf<Location>().apply {
-                add(
-                    Location(
-                        null,
-                        "None",
-                        false,
-                        null,
-                        null,
-                        "",
-                        mutableListOf<String>()
-                    )
-                )
-                addAll(newLocations)
-            }
-            locationAdapter.clear()
-            locationAdapter.addAll(modifiedLocations)
-            locationAdapter.notifyDataSetChanged()
-
-            val defaultPosition = modifiedLocations.indexOfFirst { it.id == locationId }
-
-            if (defaultPosition >= 0) {
-                spinnerLocation.setSelection(defaultPosition)
-            }
-        }
-
-        val weapons = mutableListOf<Weapon>()
-        val weaponAdapter = WeaponAdapter(requireContext(), weapons)
-        spinnerWeapon.adapter = weaponAdapter
-
-        viewModel.getAllWeapons().observe(viewLifecycleOwner) { newWeapons ->
-            val modifiedWeapons = mutableListOf<Weapon>().apply {
-                add(Weapon(null, "None", "", mutableListOf<String>()))
-                addAll(newWeapons)
-            }
-            weaponAdapter.clear()
-            weaponAdapter.addAll(modifiedWeapons)
-            weaponAdapter.notifyDataSetChanged()
-
-            val defaultPosition = modifiedWeapons.indexOfFirst { it.id == weaponId }
-
-            if (defaultPosition >= 0) {
-                spinnerWeapon.setSelection(defaultPosition)
-            }
-        }
-
-        sharedViewModel.getWeaponsUpdateSignal().observe(viewLifecycleOwner) { updated ->
-            if (updated) {
-                viewModel.getAllWeapons().observe(viewLifecycleOwner) { newWeapons ->
-                    newWeapons?.let { weapons ->
-                        weaponAdapter.clear()
-                        weaponAdapter.addAll(weapons)
-                        weaponAdapter.notifyDataSetChanged()
-                        sharedViewModel.resetWeaponsUpdatedSignal()
-                    } ?: run {
-                        sharedViewModel.resetWeaponsUpdatedSignal()
-                    }
-                }
-            }
-        }
-
-        sharedViewModel.getLocationsUpdateSignal().observe(viewLifecycleOwner) { updated ->
-            if (updated) {
-                viewModel.getAllLocations().observe(viewLifecycleOwner) { newLocations ->
-                    newLocations?.let { locations ->
-                        locationAdapter.clear()
-                        locationAdapter.addAll(locations)
-                        locationAdapter.notifyDataSetChanged()
-                        sharedViewModel.resetLocationsUpdatedSignal()
-                    } ?: run {
-                        sharedViewModel.resetLocationsUpdatedSignal()
-                    }
-                }
-            }
-        }
-
-        val imagesRecyclerView = view.findViewById<RecyclerView>(R.id.imagesAnimalRecyclerView)
-        imageAdapter = ImagesAdapter(selectedImageUris) { imageUri ->
-            val dialog = ImageDialogFragment.newInstance(imageUri)
-            dialog.show(childFragmentManager, "viewImage")
-        }
-        imagesRecyclerView.adapter = imageAdapter
-        imagesRecyclerView.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-
-
-        spinnerLocation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                val selectedLocation = parent.getItemAtPosition(position) as Location
-                locationId = selectedLocation.id
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // Handle case when nothing is selected if needed
-            }
-        }
-
-        spinnerWeapon.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                val selectedWeapon = parent.getItemAtPosition(position) as Weapon
-                weaponId = selectedWeapon.id
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // Handle case when nothing is selected if needed
-            }
-        }
+        setupSpinnerLocation(spinnerLocation)
+        setupWeaponLocation(spinnerWeapon)
+        setupImageView(imagesRecyclerView)
 
         buttonSaveAnimal.setOnClickListener {
             val name = editTextSpecieName.text.toString()
@@ -242,18 +127,117 @@ class AnimalAddFragment : Fragment() {
         })
     }
 
-    private fun showImagePicker() {
-        ImagePickerUtil.openImagePicker(this, pickImagesRequestCode)
+    private fun setupSpinnerLocation(spinnerLocation: Spinner) {
+        val locations = mutableListOf<Location>()
+        val locationAdapter = LocationAdapter(requireContext(), locations)
+        spinnerLocation.adapter = locationAdapter
+
+        viewModel.getAllLocations().observe(viewLifecycleOwner) { newLocations ->
+            val modifiedLocations = mutableListOf<Location>().apply {
+                add(Location(null, "None", false, null, null, "", mutableListOf<String>())
+                )
+                addAll(newLocations)
+            }
+            locationAdapter.clear()
+            locationAdapter.addAll(modifiedLocations)
+            locationAdapter.notifyDataSetChanged()
+
+            val defaultPosition = modifiedLocations.indexOfFirst { it.id == locationId }
+
+            if (defaultPosition >= 0) {
+                spinnerLocation.setSelection(defaultPosition)
+            }
+        }
+
+        spinnerLocation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedLocation = parent.getItemAtPosition(position) as Location
+                locationId = selectedLocation.id
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle case when nothing is selected if needed
+            }
+        }
+
+        sharedViewModel.getLocationsUpdateSignal().observe(viewLifecycleOwner) { updated ->
+            if (updated) {
+                viewModel.getAllLocations().observe(viewLifecycleOwner) { newLocations ->
+                    newLocations?.let { locations ->
+                        locationAdapter.clear()
+                        locationAdapter.addAll(locations)
+                        locationAdapter.notifyDataSetChanged()
+                        sharedViewModel.resetLocationsUpdatedSignal()
+                    } ?: run {
+                        sharedViewModel.resetLocationsUpdatedSignal()
+                    }
+                }
+            }
+        }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == pickImagesRequestCode && resultCode == Activity.RESULT_OK) {
-            val imagesUris = ImagePickerUtil.extractSelectedImages(data)
-            selectedImageUris.clear()
-            selectedImageUris.addAll(imagesUris.map { it.toString() })
-            imageAdapter.updateImages(selectedImageUris)
+    private fun setupWeaponLocation(spinnerWeapon: Spinner) {
+
+        val weapons = mutableListOf<Weapon>()
+        val weaponAdapter = WeaponAdapter(requireContext(), weapons)
+        spinnerWeapon.adapter = weaponAdapter
+
+        viewModel.getAllWeapons().observe(viewLifecycleOwner) { newWeapons ->
+            val modifiedWeapons = mutableListOf<Weapon>().apply {
+                add(Weapon(null, "None", "", mutableListOf<String>()))
+                addAll(newWeapons)
+            }
+            weaponAdapter.clear()
+            weaponAdapter.addAll(modifiedWeapons)
+            weaponAdapter.notifyDataSetChanged()
+
+            val defaultPosition = modifiedWeapons.indexOfFirst { it.id == weaponId }
+
+            if (defaultPosition >= 0) {
+                spinnerWeapon.setSelection(defaultPosition)
+            }
         }
+
+        sharedViewModel.getWeaponsUpdateSignal().observe(viewLifecycleOwner) { updated ->
+            if (updated) {
+                viewModel.getAllWeapons().observe(viewLifecycleOwner) { newWeapons ->
+                    newWeapons?.let { weapons ->
+                        weaponAdapter.clear()
+                        weaponAdapter.addAll(weapons)
+                        weaponAdapter.notifyDataSetChanged()
+                        sharedViewModel.resetWeaponsUpdatedSignal()
+                    } ?: run {
+                        sharedViewModel.resetWeaponsUpdatedSignal()
+                    }
+                }
+            }
+        }
+
+        spinnerWeapon.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val selectedWeapon = parent.getItemAtPosition(position) as Weapon
+                weaponId = selectedWeapon.id
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Handle case when nothing is selected if needed
+            }
+        }
+    }
+
+    private fun setupImageView(imagesRecyclerView: RecyclerView) {
+        imageAdapter = ImagesAdapter(selectedImageUris) { imageUri ->
+            val dialog = ImageDialogFragment.newInstance(imageUri)
+            dialog.show(childFragmentManager, "viewImage")
+        }
+        imagesRecyclerView.adapter = imageAdapter
+        imagesRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
     }
 
     companion object {
