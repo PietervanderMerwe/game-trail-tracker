5ec0426b3387511fe9dbbab602309b1f207bd82b
EPi <pietervdm64@gmail.com>
2024-04-02 11:26:26 +0200

    Refactor update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.room.runtime)
+    implementation(libs.androidx.core)
 
     // this should actually be Kapt or ksp so check if database breaks
     ksp(libs.room.compiler)
