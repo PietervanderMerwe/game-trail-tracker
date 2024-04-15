947e58a87b0877fec1718b6a63e7682d86918678
EPi <pietervdm64@gmail.com>
2024-04-15 13:02:21 +0200

    Refactor update

--- app/src/main/java/com/epilogs/game_trail_tracker/database/daos/UserSettingsDao.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/database/daos/UserSettingsDao.kt
@@ -1,11 +1,13 @@
 package com.epilogs.game_trail_tracker.database.daos
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.epilogs.game_trail_tracker.database.entities.UserSettings
 
+@Dao
 interface UserSettingsDao {
     @Insert
     suspend fun insertUserSettings(userSettings: UserSettings)
--- app/src/main/java/com/epilogs/game_trail_tracker/database/entities/UserSettings.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/database/entities/UserSettings.kt
@@ -2,6 +2,7 @@ package com.epilogs.game_trail_tracker.database.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
 @Entity
 data class UserSettings (
     @PrimaryKey(autoGenerate = true) var id: Int? = null,
