b0605211b5f7a7b8fe2a979bec811dc0c63406e2
EPi <pietervdm64@gmail.com>
2024-04-18 13:06:14 +0200

    start of adding user settings

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/settings/UserSettingsFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/settings/UserSettingsFragment.kt
@@ -1,24 +1,50 @@
 package com.epilogs.game_trail_tracker.fragments.settings
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import com.epilogs.game_trail_tracker.R
+import com.epilogs.game_trail_tracker.databinding.FragmentUserSettingsBinding
 
 class UserSettingsFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentUserSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_user_settings, container, false)
+        _binding = FragmentUserSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val colorStateList = ColorStateList(
+            arrayOf(
+                intArrayOf(-android.R.attr.state_checked),
+                intArrayOf(android.R.attr.state_checked)
+            ),
+            intArrayOf(
+                ContextCompat.getColor(requireContext(), R.color.primary_color),
+                ContextCompat.getColor(requireContext(), R.color.text_black)
+            )
+        )
+
+        binding.switchTheme.thumbTintList = colorStateList
+        binding.switchTheme.trackTintList = colorStateList
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
