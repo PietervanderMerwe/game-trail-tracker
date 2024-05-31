c49671a09d8aca2f81a561c18b05283eee62f444
Pieter-vdm <pieter.vandermerwe@trilogy.com>
2024-05-31 12:41:38 +0200

    Changed calander picker colors

--- app/src/main/java/com/epilogs/game_trail_tracker/utils/showDatePickerDialog.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/utils/showDatePickerDialog.kt
@@ -2,6 +2,7 @@ package com.epilogs.game_trail_tracker.utils
 
 import android.app.DatePickerDialog
 import android.content.Context
+import com.epilogs.game_trail_tracker.R
 import java.util.Calendar
 
 fun showDatePickerDialog(
@@ -15,7 +16,7 @@ fun showDatePickerDialog(
     val month = calendar.get(Calendar.MONTH)
     val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
 
-    val datePickerDialog = DatePickerDialog(context, { _, selectedYear, selectedMonth, selectedDay ->
+    val datePickerDialog = DatePickerDialog(context, R.style.CustomDatePickerDialogTheme,{ _, selectedYear, selectedMonth, selectedDay ->
         val selectedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
         setDate(selectedDate)
     }, year, month, dayOfMonth)
