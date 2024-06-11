a66b8b8d29dbab0c2647d738d20caa690b1a67f8
EPi <pietervdm64@gmail.com>
2024-06-11 19:02:18 +0200

    Changed date picker button colors being purple

--- app/src/main/java/com/epilogs/game_trail_tracker/utils/showDatePickerDialog.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/utils/showDatePickerDialog.kt
@@ -2,6 +2,7 @@ package com.epilogs.game_trail_tracker.utils
 
 import android.app.DatePickerDialog
 import android.content.Context
+import android.view.ContextThemeWrapper
 import com.epilogs.game_trail_tracker.R
 import java.util.Calendar
 
@@ -16,10 +17,14 @@ fun showDatePickerDialog(
     val month = calendar.get(Calendar.MONTH)
     val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
 
-    val datePickerDialog = DatePickerDialog(context, R.style.CustomDatePickerDialogTheme,{ _, selectedYear, selectedMonth, selectedDay ->
-        val selectedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
-        setDate(selectedDate)
-    }, year, month, dayOfMonth)
+    val datePickerDialog = DatePickerDialog(
+        ContextThemeWrapper(context, R.style.CustomDatePickerDialogTheme),
+        { _, selectedYear, selectedMonth, selectedDay ->
+            val selectedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+            setDate(selectedDate)
+        },
+        year, month, dayOfMonth
+    )
 
     minDate?.let {
         datePickerDialog.datePicker.minDate = it.timeInMillis
