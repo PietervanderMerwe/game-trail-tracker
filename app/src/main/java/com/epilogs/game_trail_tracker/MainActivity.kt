18dcd2bd6968bf526d079f36241deb1cd12a66aa
EPi <pietervdm64@gmail.com>
2024-06-14 21:03:45 +0200

    update weird navigation issue

--- app/src/main/java/com/epilogs/game_trail_tracker/MainActivity.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.epilogs.game_trail_tracker.database.entities.UserSettings
@@ -15,7 +17,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private var userSettings = UserSettings(id = 1, theme = "light_mode", measurement = "cm", weight = "kg")
-//    private lateinit var adView: AdView
+    private lateinit var navController: NavController
 
     private val userSettingsViewModel: UserSettingsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +27,9 @@ class MainActivity : AppCompatActivity() {
                 "dark_mode" -> setTheme(R.style.Base_Theme_Gametrailtracker) // Dark theme
                 "light_mode" -> setTheme(R.style.Base_Theme_Gametrailtracker) // Light theme
             }
-//            MobileAds.initialize(this) {}
-//
-//            adView = findViewById(R.id.adView)
-
-//            val adRequest = AdRequest.Builder().build()
-//            adView.loadAd(adRequest)
-
+            if (userSetting == null) {
+                userSettingsViewModel.insertUserSettings(userSettings)
+            }
         }
 
         super.onCreate(savedInstanceState)
@@ -45,21 +43,38 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // Find the BottomNavigationView and remove insets listener and padding
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         navView.setOnApplyWindowInsetsListener(null)
         navView.setPadding(0, 0, 0, 0)
-
-        // Setup BottomNavigationView with NavController
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        navView.setupWithNavController(navController)
-
-        // Observe user settings and insert default settings if none exist
-        userSettingsViewModel.getUserSettingsById(1).observe(this) { userSetting ->
-            if (userSetting == null) {
-                userSettingsViewModel.insertUserSettings(userSettings)
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.huntFragment -> {
+                    navigateToRootFragment(R.id.huntFragment)
+                    true
+                }
+                R.id.trophyFragment -> {
+                    navigateToRootFragment(R.id.trophyFragment)
+                    true
+                }
+                R.id.weaponFragment -> {
+                    navigateToRootFragment(R.id.weaponFragment)
+                    true
+                }
+                else -> false
             }
         }
+    }
+
+    private fun navigateToRootFragment(fragmentId: Int) {
+        val navOptions = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setRestoreState(true)
+            .setPopUpTo(navController.graph.startDestinationId, inclusive = true) // Inclusive to ensure the back stack is cleared
+            .build()
+
+        navController.navigate(fragmentId, null, navOptions)
     }
 }
