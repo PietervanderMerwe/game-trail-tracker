fb2a61a6f45bc9ebe92ec3f90c292103561f3120
EPi <pietervdm64@gmail.com>
2024-08-15 15:44:48 +0200

    Start of update for bullet add

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/bullet/BulletAddFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/bullet/BulletAddFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.epilogs.game_trail_tracker.R
+import com.epilogs.game_trail_tracker.databinding.FragmentBulletAddBinding
 
 class BulletAddFragment : Fragment() {
 
     private var weaponId: Int? = null
+    private lateinit var binding: FragmentBulletAddBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,48 @@ class BulletAddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_bullet_add, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBulletAddBinding.bind(view)
+
+        setupTabToggleBtn()
+    }
+
+    private fun setupTabToggleBtn() {
+        binding.tabToggleGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.storeBoughtToggle -> {
+                        binding.storeBoughtToggle.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.primary_color))
+                        binding.storeBoughtToggle.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+
+                        binding.handLoadedToggle.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.gray))
+                        binding.handLoadedToggle.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_black_disabled))
+
+                        setStoreBought()
+                    }
+                    R.id.handLoadedToggle -> {
+                        binding.handLoadedToggle.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.primary_color))
+                        binding.handLoadedToggle.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+
+                        binding.storeBoughtToggle.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.gray))
+                        binding.storeBoughtToggle.setTextColor(ContextCompat.getColor(requireContext(), R.color.text_black_disabled))
+
+                        setHandLoaded()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setStoreBought() {
+
+    }
+
+    private fun setHandLoaded() {
+
     }
 
     companion object {
