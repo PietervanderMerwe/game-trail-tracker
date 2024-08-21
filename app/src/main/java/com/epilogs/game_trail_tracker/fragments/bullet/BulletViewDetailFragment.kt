65a019f74e052554bbc2ea96dd78182382622438
Pieter-vdm <pieter.vandermerwe@trilogy.com>
2024-08-21 16:23:35 +0200

    Rework for bullet view details

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/bullet/BulletViewDetailFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/bullet/BulletViewDetailFragment.kt
@@ -1,12 +1,16 @@
 package com.epilogs.game_trail_tracker.fragments.bullet
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.epilogs.game_trail_tracker.FullScreenImageActivity
 import com.epilogs.game_trail_tracker.R
+import com.epilogs.game_trail_tracker.adapters.ImagesAdapter
 import com.epilogs.game_trail_tracker.database.entities.Bullet
 import com.epilogs.game_trail_tracker.databinding.FragmentBulletViewDetailBinding
 import com.epilogs.game_trail_tracker.viewmodels.BulletViewModel
@@ -18,6 +22,7 @@ class BulletViewDetailFragment : Fragment() {
     private var bulletId: Int? = null
     private lateinit var binding: FragmentBulletViewDetailBinding
     private val viewModel: BulletViewModel by viewModels()
+    private lateinit var imageAdapter: ImagesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +57,26 @@ class BulletViewDetailFragment : Fragment() {
             binding.textViewBulletWeightViewDetail.setText(bullet.bulletWeight.toString())
             binding.textViewBulletTypeViewDetail.setText(bullet.bulletType)
             binding.textViewBulletNotesViewDetail.setText(bullet.notes)
+
+            if(bullet.imagePaths?.isEmpty() == true) {
+                binding.imagesBulletRecyclerViewViewDetail.visibility = View.GONE
+            } else {
+                binding.imagesBulletRecyclerViewViewDetail.visibility = View.VISIBLE
+
+                val imagePaths = bullet.imagePaths?.toMutableList() ?: mutableListOf()
+                imageAdapter = ImagesAdapter(imagePaths) { imageUrl, position ->
+                    val intent = Intent(context, FullScreenImageActivity::class.java).apply {
+                        putStringArrayListExtra("image_urls", ArrayList(bullet.imagePaths))
+                        putExtra("image_position", position)
+                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+                    }
+                    context?.startActivity(intent)
+                }
+
+                binding.imagesBulletRecyclerViewViewDetail.adapter = imageAdapter
+                binding.imagesBulletRecyclerViewViewDetail.layoutManager =
+                    GridLayoutManager(requireContext(), 3)
+            }
         }
     }
 
