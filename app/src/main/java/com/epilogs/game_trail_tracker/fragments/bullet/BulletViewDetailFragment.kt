f7feae1c3ef952337ad23f19a1f203fe6bbfe8f4
Pieter-vdm <pieter.vandermerwe@trilogy.com>
2024-08-21 15:56:21 +0200

    Start of rework for bullet view details

--- app/src/main/java/com/epilogs/game_trail_tracker/fragments/bullet/BulletViewDetailFragment.kt
+++ app/src/main/java/com/epilogs/game_trail_tracker/fragments/bullet/BulletViewDetailFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.epilogs.game_trail_tracker.R
+import com.epilogs.game_trail_tracker.databinding.FragmentBulletViewDetailBinding
+import com.epilogs.game_trail_tracker.viewmodels.BulletViewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class BulletViewDetailFragment : Fragment() {
+
+    private var bulletId: Int? = null
+    private lateinit var binding: FragmentBulletViewDetailBinding
+    private val viewModel: BulletViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            bulletId = it.getInt("bulletId")
         }
     }
 
@@ -20,6 +30,24 @@ class BulletViewDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_bullet_view_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentBulletViewDetailBinding.bind(view)
+
+        getBullet()
+    }
+
+    private fun getBullet() {
+        viewModel.getBulletById(bulletId!!).observe(viewLifecycleOwner) { bullet ->
+            binding.locationName.text = hunt?.name
+            val startDateStr = hunt?.startDate?.let { dateFormat.format(it) } ?: "N/A"
+            val endDateStr = hunt?.endDate?.let { dateFormat.format(it) } ?: "N/A"
+            val dateRange = getString(R.string.date_range, startDateStr, endDateStr)
+            binding.dateRange.text = dateRange
+        }
     }
 
     companion object {
